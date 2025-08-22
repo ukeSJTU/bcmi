70dd4be714070358ebe84d2e6ad7f438ae74fd9c
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-08-22 11:15:53 +0200

    feat(deps): add @payloadcms/translations dependency and update lockfile

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -30,6 +30,10 @@ export default buildConfig({
       url: process.env.DATABASE_URI || path.resolve(dirname, '../data/payload.db'),
     },
   }),
+  // i18n: {
+  //   supportedLanguages: { enTranslations, zhTranslations },
+  //   fallbackLanguage: 'en'
+  // },
   // If you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // This is optional - if you don't need to do these things,
